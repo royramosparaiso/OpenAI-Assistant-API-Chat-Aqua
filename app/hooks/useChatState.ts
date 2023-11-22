6edcea364aa05e9137fa286bda8dd0d16b77d724
royramosparaiso <61093603+royramosparaiso@users.noreply.github.com>
2023-11-22 13:40:34 +0100

    .

--- app/hooks/useChatState.ts
+++ app/hooks/useChatState.ts
@@ -16,7 +16,7 @@ export const useChatState = () => {
   const [threadId, setThreadId] = useState<string | null>(null);
   const [isStartLoading, setStartLoading] = useState(false);
   const [isSending, setIsSending] = useState(false);
-  const [initialThreadMessage, setInitialThreadMessage] = useState('You are a Pirate! introduce yourself');
+  const [initialThreadMessage, setInitialThreadMessage] = useState('Introduce yourself');
   const [statusMessage, setStatusMessage] = useState('');
   const counter = useRef(0);
   const inputRef = useRef(null);
